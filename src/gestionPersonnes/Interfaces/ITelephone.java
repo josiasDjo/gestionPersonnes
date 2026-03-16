70d0d9b435f5c5052ff3f1a7a3f15f5194c3308b
Nyabahama <chrispinnyabahama@gmail.com>
2026-03-16 21:14:34 +0200

    yes
    yes

--- src/gestionPersonnes/Interfaces/ITelephone.java
+++ src/gestionPersonnes/Interfaces/ITelephone.java
@@ -1,5 +1,16 @@
-package gestionPersonnes.Interfaces;
+package com.groupe3.projects.interfaces;
+
+import com.groupe3.projects.model.Telephone;
 
 public interface ITelephone {
 
+    int nouveau();
+
+    int enregistrer(Telephone telephone);
+
+    int supprimer(int id);
+    
+    Telephone getTelephone(int id);
+
+    String toString();
 }
